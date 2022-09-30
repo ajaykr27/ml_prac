bf1887a8bef39b13034ea1a8ec3ee3520230e780
ajaykr27 <64725464+ajaykr27@users.noreply.github.com>
2022-09-30 18:02:31 +0530

    Add files via upload

---
+++ src/app/Models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column,Integer, Float, String
+from Database import Base
+
+class Notes(Base):
+    __tablename__= "banknote1"
+
+    Id = Column(Integer,primary_key=True,index=True)
+    variance = Column(Float)
+    skewness = Column(Float)
+    curtosis = Column(Float)
+    entropy = Column(Float)
+    note_class = Column(Integer)
+    prediction = Column(String(40))
